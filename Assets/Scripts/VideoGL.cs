d23d75c0462ad3ec7c727e9a967cbc6213ade76b
nikki <moniqshiin@gmail.com>
2024-12-04 15:18:13 +0800

    WEBGL 內容上傳

--- Assets/Scripts/VideoGL.cs
+++ Assets/Scripts/VideoGL.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Video;
@@ -5,15 +6,37 @@ using UnityEngine.Video;
 public class VideoGL : MonoBehaviour
 {
     public VideoPlayer Wall;
-    public string fileWall;
     public VideoPlayer Floor;
-    public string fileFloor;
+    public List<string> VideoNameWall;
+    public List<string> VideoNameGround;
     void Start()
     {
-        Wall.url = Path.Combine(Application.streamingAssetsPath, fileWall);
+        SetupPlay(0);
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SetupPlay(0);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            SetupPlay(1);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)){
+            SetupPlay(2);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)){
+            SetupPlay(3);
+        }
+    }
+
+    void SetupPlay(int index){
+        Wall.Stop();
+        Floor.Stop();
+
+        Wall.url = Path.Combine(Application.streamingAssetsPath, VideoNameWall[index]);
         Wall.Play();
 
-        Floor.url = Path.Combine(Application.streamingAssetsPath, fileFloor);
+        Floor.url = Path.Combine(Application.streamingAssetsPath, VideoNameGround[index]);
         Floor.Play();
     }
 }
